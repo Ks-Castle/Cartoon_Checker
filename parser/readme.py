98ccbcbcf555e059c9a299b5657d5e6596b7f86d
kevinkim910408 <junho2413@gmail.com>
2023-01-22 12:17:55 -0500

    @ test

--- parser/readme.py
+++ parser/readme.py
@@ -22,7 +22,7 @@ for page_number in data:
     i += 1
    
 def inject_result_to_readme():
-    with open('../Readme.md') as readme:
+    with open('../README.md') as readme:
         timestamp = get_timestamp()
         title01 = [s for s in arr if "원피스" in s['title']][0]['title']
         title02 = [s for s in arr if "원펀맨" in s['title']][0]['title']
@@ -35,7 +35,7 @@ def inject_result_to_readme():
         
         updated_content = ( f'> Auto Updated Date: {timestamp}\n\n ### 01.{title01}, {time01[0:5]} \n ### 02.{title02}, {time02[0:5]} \n ### 03.{title03}, {time03[0:5]} \n ### 04.{title04}, {time04[0:5]} ')
         print('readme update is okay')
-    with open('../Readme.md', 'w') as readme:
+    with open('../README.md', 'w') as readme:
         readme.write(updated_content)
         
         
