3ab9fe349819bb23408d37ecc87592be8eb8a683
kevinkim910408 <junho2413@gmail.com>
2023-01-22 05:44:55 -0500

    @ Data fetching and saving

---
+++ parser/api.py
@@ -0,0 +1,45 @@
+import requests
+import bs4
+
+def get_info():
+    whole_source= ""
+    arr = []
+    i = 0
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
+        "Accept": "text_html,application_xhtml+xml,application_xml;q=0.9,image_webp,**/**;q=0.8"
+    }
+    for page_number in range(1, 21):
+        URL = "http://11toon61.com/bbs/board.php?bo_table=toon_c&is=0&sord=0&type=upd&page=" + str(page_number)
+        response = requests.get(URL, headers=headers)
+        whole_source = whole_source + response.text
+        soup = bs4.BeautifulSoup(whole_source, 'html.parser')
+        title = soup.select("#free-genre-list > li > a > div.homelist-title > span")
+        time = soup.select("#free-genre-list > li > a > div.homelist-genre > span")
+        while i < len(title) - 1:
+            i += 1
+            arr.append({
+            'title': title[i].text,
+            'time': time[i].text
+        })
+    return arr
+
+ 
+    
+# for i, info in enumerate(info, 2):
+#     title = info.select_one('div.homelist-title > span')
+# print(title)
+# imageURL:
+# updatedAt:
+# linkURL:
+
+    
+# print(test)
+# if True:
+#     arr.append({
+#         'id': i,
+#         'title' : a.text,
+#         'url': 'https://movie.naver.com' + a['href'],
+#         'icon': icon['src'],
+#         'range': range.text
+#     })
---
+++ parser/run.py
@@ -0,0 +1,21 @@
+import json
+from pprint import pprint
+from time import sleep as delay
+from api import get_info
+
+
+if __name__ == '__main__':
+    data = get_info()
+    print(data)
+    delay(0.1)
+    updated = False
+    if len(data):
+        updated = True
+    if updated:
+        with open('../mockup/data.json', 'w') as product_file:
+            print(product_file)
+            json.dump(data, product_file, ensure_ascii=False, indent=2)
+            product_file.write('\n')
+     
+    pprint(data)
+    print('Update !')
