af18507c4bd70f9da4974b16af38d4a1a8494b56
kevinkim910408 <junho2413@gmail.com>
2023-01-22 10:29:16 -0500

    @ Client Header View

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 const Header = () => {
-  return <header className="p-5 bg-blue-500"></header>;
+  const [time, setTime] = useState(new Date());
+  const [updateTime, setUpdateTime] = useState<{
+    hour: string;
+    minute: string;
+  }>({
+    hour: `${12 - time.getHours()}`,
+    minute: `${60 - time.getMinutes()}`,
+  });
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+
+  useEffect(() => {
+    if (time.getHours() < 12) {
+      setUpdateTime({
+        hour: `${11 - time.getHours()}`,
+        minute: `${60 - time.getMinutes()}`,
+      });
+    } else {
+      setUpdateTime({
+        hour: `${35 - time.getHours()}`,
+        minute: `${60 - time.getMinutes()}`,
+      });
+    }
+  }, [time]);
+
+  return (
+    <header className="p-5 bg-blue-300 text-center">
+      <p className="text-[2.5rem]">Update Checker</p>
+      <p className="subTitle">1시간 마다 업데이트 예정</p>
+      <strong>
+        (업데이트까지 {updateTime.hour}시간 {updateTime.minute}분 남았습니다.)
+      </strong>
+    </header>
+  );
 };
 
 export default Header;
--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,86 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import styles from "./App.module.css";
 import data from "../mockup/data.json";
 
 const Home = () => {
-  const [time, setTime] = useState(new Date());
-  const [updateTime, setUpdateTime] = useState<{
-    hour: string;
-    minute: string;
-  }>({
-    hour: `${12 - time.getHours()}`,
-    minute: `${60 - time.getMinutes()}`,
-  });
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
-
-  useEffect(() => {
-    if (time.getHours() < 12) {
-      setUpdateTime({
-        hour: `${11 - time.getHours()}`,
-        minute: `${60 - time.getMinutes()}`,
-      });
-    } else {
-      setUpdateTime({
-        hour: `${35 - time.getHours()}`,
-        minute: `${60 - time.getMinutes()}`,
-      });
-    }
-  }, [time]);
-
-  const dayArr = ["일", "월", "화", "수", "목", "금", "토"];
-
-  const today =
-    time.getFullYear() +
-    "년 " +
-    (time.getMonth() + 1) +
-    "월 " +
-    time.getDate() +
-    "일 " +
-    dayArr[time.getDay()] +
-    "요일 ";
-
   return (
-    <div className={styles.wrap}>
-      <div className={styles.flexBox}>
-        <p className={styles.title}>네이버 영화랭킹</p>
-        <p className={styles.subTitle}>하루 한번 업데이트</p>
-        <p className={styles.subTitle}>17:00 UTC / 12:00 EST / 2:00AM GMT</p>
-        <p>
-          <strong>
-            (현재시간: {today} - {time.toLocaleTimeString()})
-          </strong>
-        </p>
-        <p className={styles.subTitle}>
-          <strong>
-            (업데이트까지 {updateTime.hour}시간 {updateTime.minute}분
-            남았습니다.)
-          </strong>
-        </p>
+    <div className="wrap">
+      <div className="flexBox">
+        <p className="subTitle"></p>
       </div>
-      {data.length === 0 && <div>데이터가 없습니다</div>}
-      <div className={styles.main}>
+      {data.length === 0 && <p>데이터가 없습니다</p>}
+      <div className="main">
         <ul>
           {data.map((v: any, i) => {
             return (
-              <a href={v.url} target="_blank" className={styles.url} key={i}>
-                <li className={styles.content}>
+              <a href={v.link} target="_blank" className="url" key={i}>
+                <li className="content">
                   <div>
                     {i + 1}: {v.title}
                   </div>
-                  <div className={styles.flexRow}>
-                    <img
-                      src={v.icon}
-                      alt=""
-                      style={{ height: "13px", marginRight: "0.5rem" }}
-                    />
-                    <p>{v.range}</p>
-                  </div>
+                  <p>
+                    마지막 업데이트: &nbsp;
+                    <span style={{ color: `red` }}>
+                      {v.time[v.time.length - 1].substr(0, 5)}
+                    </span>
+                  </p>
                 </li>
               </a>
             );
           })}
         </ul>
       </div>
+      <style jsx>{`
+        .wrap {
+          background: rgb(237, 178, 231);
+          background: linear-gradient(
+            80deg,
+            rgba(237, 178, 231, 1) 0%,
+            rgb(249, 253, 228) 100%
+          );
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .main {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 2rem;
+        }
+        .content {
+          font-size: 1.2rem;
+          margin: 1rem;
+          border: 1px solid black;
+          padding: 1rem;
+          display: flex;
+          flex-direction: column;
+        }
+        .content:hover {
+          border: 3px solid black;
+        }
+        .link:hover {
+          opacity: 50%;
+        }
+        .flexBox {
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          align-items: center;
+        }
+        .flexRow {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        .subTitle:nth-of-type(2) {
+          font-size: 1.5rem;
+          color: rgb(240, 110, 132);
+        }
+      `}</style>
     </div>
   );
 };
