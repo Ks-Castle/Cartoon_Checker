e31019366383f1c4fd1ef444b8d6eba501473f29
kevinkim910408 <junho2413@gmail.com>
2023-01-22 10:11:57 -0500

    @ test

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,96 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import styles from "./App.module.css";
+import data from "../mockup/data.json";
 
 const Home = () => {
-  /*
-  1. 파이썬 bs4 크롤링 -> 이미지, 몇일전, 이름, 작가
-  2. save it as json file
-  3. front: fetch and display
-  4. crone bot 하루 4번 24, 6, 12 , 18
+  const [time, setTime] = useState(new Date());
+  const [updateTime, setUpdateTime] = useState<{
+    hour: string;
+    minute: string;
+  }>({
+    hour: `${12 - time.getHours()}`,
+    minute: `${60 - time.getMinutes()}`,
+  });
 
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
 
-  */
-  return <div className="text-red-500">This is Home</div>;
+  useEffect(() => {
+    if (time.getHours() < 12) {
+      setUpdateTime({
+        hour: `${11 - time.getHours()}`,
+        minute: `${60 - time.getMinutes()}`,
+      });
+    } else {
+      setUpdateTime({
+        hour: `${35 - time.getHours()}`,
+        minute: `${60 - time.getMinutes()}`,
+      });
+    }
+  }, [time]);
+
+  const dayArr = ["일", "월", "화", "수", "목", "금", "토"];
+
+  const today =
+    time.getFullYear() +
+    "년 " +
+    (time.getMonth() + 1) +
+    "월 " +
+    time.getDate() +
+    "일 " +
+    dayArr[time.getDay()] +
+    "요일 ";
+
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.flexBox}>
+        <p className={styles.title}>네이버 영화랭킹</p>
+        <p className={styles.subTitle}>하루 한번 업데이트</p>
+        <p className={styles.subTitle}>17:00 UTC / 12:00 EST / 2:00AM GMT</p>
+        <p>
+          <strong>
+            (현재시간: {today} - {time.toLocaleTimeString()})
+          </strong>
+        </p>
+        <p className={styles.subTitle}>
+          <strong>
+            (업데이트까지 {updateTime.hour}시간 {updateTime.minute}분
+            남았습니다.)
+          </strong>
+        </p>
+      </div>
+      {data.length === 0 && <div>데이터가 없습니다</div>}
+      <div className={styles.main}>
+        <ul>
+          {data.map((v: any, i) => {
+            return (
+              <a href={v.url} target="_blank" className={styles.url} key={i}>
+                <li className={styles.content}>
+                  <div>
+                    {i + 1}: {v.title}
+                  </div>
+                  <div className={styles.flexRow}>
+                    <img
+                      src={v.icon}
+                      alt=""
+                      style={{ height: "13px", marginRight: "0.5rem" }}
+                    />
+                    <p>{v.range}</p>
+                  </div>
+                </li>
+              </a>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
